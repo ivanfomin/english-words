ae3b426a7e920b360a24f35784fe7b81b92f82dc
Ivan Fomin <la-verne@mail.ru>
2019-11-15 22:35:03 +0300

    Add Db modifying 2

--- src/Core/ConfirmUser.php
+++ src/Core/ConfirmUser.php
@@ -18,7 +18,7 @@ class ConfirmUser
         $this->user->save();
         $to = "<" . $this->user->email . ">";
         $subject = 'Подтверждение регистрации';
-        $message = '<p> Перейдите по ссылке для подтверждения регистрации</p> <a href="https://wordsenglish/Friends/Confirm/' . $rand . '">Подтвердить регистрации на wordsenglish.ru</a>  </br>
+        $message = '<p> Перейдите по ссылке для подтверждения регистрации</p> <a href="https://wordsenglish.ru/Friends/Confirm/' . $rand . '">Подтвердить регистрации на wordsenglish.ru</a>  </br>
             <p>Если Вы этого не делали, пожалуйста игнорируйте это письмо</p>';
         $headers = "Content-type: text/html; charset=utf-8 \r\n";
         $headers .= "From: cn17628@pingin.ru \r\n";
@@ -29,7 +29,7 @@ class ConfirmUser
     {
         $to = "<" . $this->user->email . ">";
         $subject = 'Восстановление пароля';
-        $message = '<p> Перейдите по ссылке для восстановления пароля</p> <a href="https://wordsenglish/Friends/Restore/' . $this->user->id . '">Перейти для восстановления пароля на wordsenglish.ru</a></p></br>
+        $message = '<p> Перейдите по ссылке для восстановления пароля</p> <a href="https://wordsenglish.ru/Friends/Restore/' . $this->user->id . '">Перейти для восстановления пароля на wordsenglish.ru</a></p></br>
                 <p>Если Вы этого не делали, пожалуйста игнорируйте это письмо</p>';
         $headers = "Content-type: text/html; charset=utf-8 \r\n";
         $headers .= "From: cn17628@pingin.ru \r\n";
